337b6fb1dfd51f102e1a19719f5a4ab9dbb928f6
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-09 20:38:26 -0500

    NT1 - feet - se implementa algoritmo de datos primitivos y suma desde consola

---
+++ main.py
@@ -0,0 +1,16 @@
+#variables primitivas con phyton
+#nombres especificos
+#debo declarar y asignar,  o si no tiene aun el valor igualarla a none
+#asignarle el valor auna variable es "quemar una variable"
+nombreEstudiante="Natalia Duarte"
+edadEstudiante =31
+estaturaEstudiante=1.52
+#los boleano empiezan en mayuscula
+inchadeAlgunEquipo=False
+
+#toda variable inicia en minuscula
+#pedir datos del teclado
+comidafavorita=input("Digita tu comida favorita:")
+#mostar datos en pantalla (salida)
+#f"" fprint investigar - y pone el mensaje y para concatenar abre llaves
+print(f"Hola {nombreEstudiante} tu comida favorita es {comidafavorita}")
