84eae82a7753734734878c1dc6b8749df3526973
Natalia Duarte <132314009+Duartestuff@users.noreply.github.com>
2024-01-16 20:28:32 -0500

    clase de hoy revisar

---
+++ proyectos phyton/plataforma_siembra_de_arboles/listas.py
@@ -0,0 +1,29 @@
+#todaws listas se debe escribir en plurak
+arboles=[]
+cantidades=[]
+lluviaMayoreasA500=[]
+
+#Recorriendo un arreglo...
+#Acceder de forma dinamica al cont6enido de un arreglo
+#para rrecorrerlo debo utilizar un ciclo o un bucle o looop
+
+#ciclo whilw (mientras)
+
+ #variables auxiliares que se usan para regular los ciclos
+
+#ciclo while (mientras)
+'''contador=0
+while contador < 10 : 
+    contador=contador+1
+    print("estoy triunfando...")'''
+
+#ciclo for (para)
+
+for variableiteradora in range(10):
+    print("variable iteradora")
+
+for i in range(10,50):
+     print(i)
+
+for i in range(1,301,2):
+    print(i)
